d66cfea5ea6242987bd7fdf607b84647518f6062
zerdakosmin <37200853+zerdakosmin@users.noreply.github.com>
2019-02-17 15:21:51 -0800

    Specify PORT

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import os
 import scrape
 
 app = Flask(__name__)
@@ -13,4 +14,5 @@ def index_post():
     return render_template('index.html', output=output)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
